228d589359511615f8f70f88ec4e9f0822b087e5
Lei-chan <hirosebirosepirose01@yahoo.co.jp>
2026-03-16 18:05:44 +0900

    Updated

--- src/app/api/history/route.ts
+++ src/app/api/history/route.ts
@@ -1,31 +1,56 @@
 import { CLIENT_URL } from "@/app/lib/config";
 import dbConnect from "@/app/lib/database";
+import {
+  HistoryContent,
+  HistoryData,
+  ImageData,
+  RegisterHistoryContent,
+  RegisterHistoryImage,
+} from "@/app/lib/definitions";
 import HistoryAmavin from "@/app/lib/models/HistoryAmavin";
 import HistoryKiyos from "@/app/lib/models/HistoryKiyos";
 import { NextResponse } from "next/server";
 
-const corsHeaders = {
-  "Access-Control-Allow-Origin": CLIENT_URL,
-  "Access-Control-Allow-Methods": "GET,OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type",
-};
+// const corsHeaders = {
+//   "Access-Control-Allow-Origin": CLIENT_URL,
+//   "Access-Control-Allow-Methods": "GET,OPTIONS",
+//   "Access-Control-Allow-Headers": "Content-Type",
+// };
 
-export async function OPTIONS() {
-  return new NextResponse(null, {
-    status: 204,
-    headers: corsHeaders,
+// export async function OPTIONS() {
+//   return new NextResponse(null, {
+//     status: 204,
+//     headers: corsHeaders,
+//   });
+// }
+
+const convertHistoryDataWithBase64 = (data: HistoryData[]) =>
+  data.map((d) => {
+    return {
+      ...d,
+      contents: d.contents.map((con: HistoryContent) => {
+        return {
+          ...con,
+          images: con.images.map((img) => {
+            return { name: img.name, data: img.buffer.toString("base64") };
+          }),
+        };
+      }),
+    };
   });
-}
 
 export async function GET() {
   await dbConnect();
 
-  const historyKiyos = await HistoryKiyos.find();
+  const historyKiyos = await HistoryKiyos.find().lean();
   const historyAmavin = await HistoryAmavin.find();
 
-  const data = { historyKiyos, historyAmavin };
+  const historyKiyosWithBase64 = convertHistoryDataWithBase64(historyKiyos);
+  const historyAmavinWithBase64 = convertHistoryDataWithBase64(historyAmavin);
+
+  const data = { historyKiyosWithBase64, historyAmavinWithBase64 };
 
   return NextResponse.json(data, {
-    headers: corsHeaders,
+    headers: { "Access-Control-Allow-Origin": CLIENT_URL },
   });
 }
--- src/app/lib/config.ts
+++ src/app/lib/config.ts
@@ -3,4 +3,4 @@ export const MIN_PASSWORD_LENGTH = 10;
 export const MIN_EACH_PASSWORD = 1;
 // export const CLIENT_URL = "https://kiyos-artisan.vercel.app/";
 // For dev
-export const CLIENT_URL = "http://localhost:3000";
+export const CLIENT_URL = "http://localhost:3000/";
