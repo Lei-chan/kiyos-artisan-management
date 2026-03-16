77524c725d953fbcae9cb5e6e2325ae01e6882d8
Lei-chan <hirosebirosepirose01@yahoo.co.jp>
2026-03-16 17:02:15 +0900

    Updated

--- src/app/lib/config.ts
+++ src/app/lib/config.ts
@@ -1,4 +1,6 @@
 export const MIN_USERNAME_LENGTH = 10;
 export const MIN_PASSWORD_LENGTH = 10;
 export const MIN_EACH_PASSWORD = 1;
-export const CLIENT_URL = "https://kiyos-artisan.vercel.app/";
+// export const CLIENT_URL = "https://kiyos-artisan.vercel.app/";
+// For dev
+export const CLIENT_URL = "http://localhost:3000";
