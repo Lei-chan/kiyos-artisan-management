ae4aa348473aeaac15f1552b50c11c0e94177ea0
Lei-chan <hirosebirosepirose01@yahoo.co.jp>
2026-03-19 09:18:28 +0900

    Added metadata baseurl

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { BASE_URL } from "./lib/config";
 
 export const metadata: Metadata = {
   title: "Kiyos Cellar & Artisan Mariage Vineyards管理者ページ",
   description: "Kiyos CellarとArtisan Mariage Vineyardsの管理者ページです。",
+  metadataBase: new URL(BASE_URL),
 };
 
 export default function RootLayout({
--- src/app/lib/config.ts
+++ src/app/lib/config.ts
@@ -1,6 +1,7 @@
 export const MIN_USERNAME_LENGTH = 10;
 export const MIN_PASSWORD_LENGTH = 10;
 export const MIN_EACH_PASSWORD = 1;
+export const BASE_URL = "https://kiyos-amavin-management.lei-chan.website/";
 export const CLIENT_URL = "https://kiyos-artisan.vercel.app/";
 // For dev
 // export const CLIENT_URL = "http://localhost:3000/";
